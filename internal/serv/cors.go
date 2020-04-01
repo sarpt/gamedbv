9db38cf14f2c2d9a1254650304600417c6a5c58a
sarpt <mike.m.info@gmail.com>
2020-04-01 02:27:20 +0200

    Fixed response to OPTIONS method including full payload, resulting in the payload being transmitted twice - first during OPTIONS request and second time during proper request

--- internal/serv/cors.go
+++ internal/serv/cors.go
@@ -2,11 +2,17 @@ package serv
 
 import "net/http"
 
+const corsOptionsMethod string = "OPTIONS"
+
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
 		resp.Header().Set("Access-Control-Allow-Origin", "*")
 		resp.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		resp.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Method")
+
+		if req.Method == corsOptionsMethod {
+			return
+		}
 
 		next.ServeHTTP(resp, req)
 	})
